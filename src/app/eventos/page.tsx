259e39bd4058c0b61f4ce320fb7de74feac446e3
Jonathan Venturim <venturim.dias@gmail.com>
2023-08-09 14:30:13 -0300

    add icon card + add evento

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -3,14 +3,27 @@ import React from 'react'
 import { ListaEventos } from '@/services/Evento'
 import moment from 'moment';
 import Link from 'next/link';
+import { CasalIcon, UsersIcon } from '@/components/svg/Icons';
 
 export default function EventoList() {
 
+  const listaOrd = ListaEventos.sort((a, b) => {
+    if(a.date > b.date){
+      return 1
+    }
+    if(a.date < b.date){
+      return -1
+    }
+    return 0
+  })
+
   return (<div className='flex flex-wrap p-[20px] gap-10 justify-center'>
-    {ListaEventos?.map(i => <Link 
+    {listaOrd?.map(i => <Link 
       href={"/eventos/"+ i.slug} 
-      className='w-[300px] border-[1px] border-white border-solid p-5'>
-      
+      className='w-[300px] border-[1px] border-white border-solid p-5 rounded-md'>
+      <div className='w-24 h-24 mb-3'>
+        {i.isCasal ? <CasalIcon /> : <UsersIcon />}
+      </div>
       <h4 className="leading-[120%] mb-5">{i.title}</h4>
       <b className='text-amber-400'>{i.local}</b>
       <p>{moment(i.date).format('DD/MM/YYYY')}</p>
--- src/services/Evento.tsx
+++ src/services/Evento.tsx
@@ -4,7 +4,8 @@ export interface EventoProps {
   title: string,
   slug: string,
   local: string,
-  img?: string
+  img?: string,
+  isCasal?: boolean
 }
 export const ListaEventos: EventoProps[] = [
   { 
@@ -12,27 +13,39 @@ export const ListaEventos: EventoProps[] = [
     title: "CHURRASCADA GAÚCHA",
     slug: 'churrascada-gaucha',
     local: 'STEFFEN CENTRO DE EVENTOS',
-    img: "https://www.steffencentrodeeventos.com.br/wp-content/uploads/2023/06/banner-site-3.png" 
+    img: "https://www.steffencentrodeeventos.com.br/wp-content/uploads/2023/06/banner-site-3.png",
+    isCasal: false,
   },
   {
     date: "2023-11-02",
     title: "COROA VERMELHA", 
     slug: 'coroa-vermelha',
-    local: 'Porto seguro - Bahia',
-    img: "/coroa.png"
+    local: 'Porto seguro - BA',
+    img: "/coroa.png",
+    isCasal: false,
   },
   { 
     date: "2023-08-25",
     title: "SESC WINE FESTIVAL", 
     slug: 'sesc-wine',
     local: 'Domingo Martins - Pedra Azul',
-    img: ""
+    img: "",
+    isCasal: true,
   },
   {
     date: "2024-04-05",
     title: "EXCURSÃO BÚZIOS", 
     slug: 'buzios',
     local: 'Búzios - RJ',
-    img: ""
+    img: "",
+    isCasal: true,
+  },
+  {
+    date: "2024-10-14",
+    title: "ENCONTRO DOS MURUCI", 
+    slug: 'encontro-dos-murucis',
+    local: 'Guaçuí - ES',
+    img: "",
+    isCasal: true,
   }
 ]
