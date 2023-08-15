1ac9a03ac30db5d1bc904523a5e9b08d4cbaf829
Jonathan Venturim <venturim.dias@gmail.com>
2023-08-15 20:01:43 -0300

    fix config media

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'media.graphassets.com',
         port: '',
-        pathname: '',
+        pathname: '/**',
       },
     ],
   },
